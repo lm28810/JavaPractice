3dd589216d690957993b31ba0b976499fbeeb848
Jazmine <lm28810@gmail.com>
2022-12-01 13:51:58 -0500

    QandA

---
+++ script.js
@@ -0,0 +1,17 @@
+// A.    Q + A
+// How do we assign a value to a variable ?
+//     with the let variable = value
+// How do we change the value of a variable ?
+//     with variable = value or variable += value
+// How do we assign an existing variable to a new variable ?
+//     with let new variable = old variable 
+// Remind me, what are declare, assign, and define ?
+//     Declare = to define something like a variable or parameter
+//     Assign = to declare a valie belngs to a variable 
+//     Define = Give meaning
+// What is pseudocoding and why should you do it ?
+//     Pseudocoding is coding in simple terms. We use it to break down logic
+//What percentage of time should be spent thinking about how you're going to solve a problem vs actually typing in code to solve it?
+//90%
+
+
